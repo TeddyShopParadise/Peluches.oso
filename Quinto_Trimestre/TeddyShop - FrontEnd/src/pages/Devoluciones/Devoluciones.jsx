439f1a21db86f0160a1a7b0ea9865d1866a75435
Jordan <jordanlozano1123@gmail.com>
2025-02-20 17:12:15 -0500

    feat(front-end): a general redesign is carried out

--- Quinto_Trimestre/TeddyShop - FrontEnd/src/pages/Devoluciones/Devoluciones.jsx
+++ Quinto_Trimestre/TeddyShop - FrontEnd/src/pages/Devoluciones/Devoluciones.jsx
@@ -155,29 +155,13 @@ const Devoluciones = () => {
     });
 
     return (
-        <Box
-            sx={{
-                height: 'auto',
-                width: '100vw',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                textAlign: 'center',
-                margin: 0,
-                padding: 0,
-                py: 2,
-            }}
-        >
-            <Box
+        <Box className="BoxInicial">
+            <Box className="Box"
                 sx={{
                     width: '90%',
                     maxWidth: '100%',
                     padding: '50px',
-                    background: 'linear-gradient(135deg, rgba(150, 50, 150, 0.9), rgba(221, 160, 221, 0.5), rgba(150, 50, 150, 0.9), rgba(255, 182, 193, 0.7))',
                     borderRadius: '30px',
-                    boxShadow: '0 5px 15px rgba(0, 0, 0, 0.5)',
-                    backdropFilter: 'blur(8px)',
-                    animation: 'shimmer 10s infinite linear',
                 }}
             >
                 <Container>
