7d26bf8380eef853ee4303c20f3854f52a365605
MaGs-Zz <miguelgomezan439@gmail.com>
2025-03-12 16:03:46 -0500

    feat(back-end): Update Inventario controller

--- Sexto_Trimestre/src/main/kotlin/Models/Productos/Inventario.kt
+++ Sexto_Trimestre/src/main/kotlin/Models/Productos/Inventario.kt
@@ -1,9 +1,9 @@
 package Models.Productos
 
-class Inventario (private var IdInventario: Int, private var StockMinimo: Int, private var PrecioVenta: Double, private var PrecioCompra: Double, private var Stock:  Int, private var StockMaximo: Int){
+class Inventario (private var StockMinimo: Int, private var PrecioVenta: Double, private var PrecioCompra: Double, private var Stock:  Int, private var StockMaximo: Int){
 
-    fun Inventario(idInventario: Int, stockMinimo: Int, precioVenta: Double, precioCompra: Double, stock: Int, stockMaximo: Int ){
-        this.IdInventario = idInventario
+    fun Inventario(stockMinimo: Int, precioVenta: Double, precioCompra: Double, stock: Int, stockMaximo: Int ){
+        //this.IdInventario = idInventario
         this.StockMinimo = stockMinimo
         this.PrecioVenta = precioVenta
         this.PrecioCompra = precioCompra
@@ -11,9 +11,9 @@ class Inventario (private var IdInventario: Int, private var StockMinimo: Int, p
         this.StockMaximo = stockMaximo
     }
 
-    fun getIdInventario(): Int{
+    /*fun getIdInventario(): Int{
         return IdInventario
-    }
+    }*/
 
     fun getStockMinimo(): Int {
         return StockMinimo
