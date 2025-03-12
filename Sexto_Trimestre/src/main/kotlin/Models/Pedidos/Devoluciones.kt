56dbbaa12ac71ada0c22d04c249cc360f27980d9
Andres-Espinosa <amdresesp21@gmail.com>
2025-03-12 15:01:15 -0500

    feat(back-end): Add setters to Clientes and Devoluciones models; update controllers

--- Sexto_Trimestre/src/main/kotlin/Models/Pedidos/Devoluciones.kt
+++ Sexto_Trimestre/src/main/kotlin/Models/Pedidos/Devoluciones.kt
@@ -1,17 +1,25 @@
 package Models.Pedidos
 
-class Devoluciones (private var IdDevolucion : Int, private var DetalleDevolucion : String){
+class Devoluciones (/*private var IdDevolucion : Int,*/ private var DetalleDevolucion : String){
 
-    fun Devoluciones(idDevolucion: Int, detalleDevolucion: String){
-        this.IdDevolucion = idDevolucion
+    fun Devoluciones(/*idDevolucion: Int,*/ detalleDevolucion: String){
+        //this.IdDevolucion = idDevolucion
         this.DetalleDevolucion = detalleDevolucion
     }
 
-    fun getIdDevolucion(): Int{
-        return IdDevolucion
-    }
+    //fun getIdDevolucion(): Int{
+    //    return IdDevolucion
+    //}
 
     fun getDetalleDevolucion(): String {
         return DetalleDevolucion
+    }
+
+    //fun setIdDevolucion(idDevolucion: Int){
+    //    this.IdDevolucion = idDevolucion
+    //}
+
+    fun setDetalleDevolucion(detalleDevolucion: String){
+        this.DetalleDevolucion = detalleDevolucion
     }
 }
