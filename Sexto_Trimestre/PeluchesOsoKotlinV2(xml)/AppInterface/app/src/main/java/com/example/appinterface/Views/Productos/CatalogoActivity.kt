481631dae22d13e16ec6b550d888a41b5ceeeed8
juanjop <jjpea406@misena.edu.co>
2025-03-19 16:59:11 -0500

    feat(back-end): Implements button delete to catalogo

--- Sexto_Trimestre/PeluchesOsoKotlinV2(xml)/AppInterface/app/src/main/java/com/example/appinterface/Views/Productos/CatalogoActivity.kt
+++ Sexto_Trimestre/PeluchesOsoKotlinV2(xml)/AppInterface/app/src/main/java/com/example/appinterface/Views/Productos/CatalogoActivity.kt
@@ -1,5 +1,6 @@
 package com.example.appinterface.Views.Productos
 import Controller.Productos.CatalogoController
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
@@ -7,6 +8,8 @@ import android.widget.Switch
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.appinterface.R
+
+
 
 class CatalogoActivity : AppCompatActivity(){
     private val catalogoController = CatalogoController()
@@ -32,4 +35,34 @@ class CatalogoActivity : AppCompatActivity(){
         val textViewCatalogos = findViewById<TextView>(R.id.textViewListado)
         textViewCatalogos.text = catalogosTexto
     }
+
+    fun eliminarCatalogo(v: View) {
+        // Crear el EditText donde el usuario ingresará el nombre del catálogo a eliminar
+        val editText = EditText(this)
+        editText.hint = "Ingresa el nombre del catálogo"
+
+        // Crear el AlertDialog
+        val alertDialog = AlertDialog.Builder(this)
+            .setTitle("Eliminar Catálogo")
+            .setMessage("Por favor ingresa el nombre exacto del catálogo a eliminar.")
+            .setView(editText)
+            .setPositiveButton("Eliminar") { dialog, which ->
+                val nombreCatalogo = editText.text.toString()
+
+                if (nombreCatalogo.isNotEmpty()) {
+                    val mensaje = catalogoController.eliminarCatalogo(nombreCatalogo)
+                    findViewById<TextView>(R.id.textViewCatalogo).text = mensaje
+                } else {
+                    findViewById<TextView>(R.id.textViewCatalogo).text = "Por favor ingresa un nombre válido."
+                }
+            }
+            .setNegativeButton("Cancelar") { dialog, which ->
+                dialog.dismiss() // Cerrar el diálogo si se cancela
+            }
+            .create()
+
+        // Mostrar el diálogo
+        alertDialog.show()
+    }
+
 }
